239c8c91459cbdccb5d2833786a8f7cc539c6f4a
Wouter Wijsman <wouter.wijsman@windesheim.nl>
2019-12-06 23:39:43 +0100

    Big update to base code

--- controllers/docker_controller.py
+++ controllers/docker_controller.py
@@ -1,13 +1,29 @@
 import docker
+import threading
 
 
 class DockerController:
 
     def __init__(self):
-        self.client = docker.from_env()
+        self.__client = docker.from_env()
 
     def get_images(self):
-        return self.client.images.list()
+        return self.__client.images.list()
 
     def get_containers(self):
-        return self.client.containers.list()
+        return self.__client.containers.list()
+
+    def run(self, image, name):
+        thread = threading.Thread(
+            target=self.__create_container,
+            args=(image, name)
+        )
+        thread.start()
+
+    def __create_container(self, image, name):
+        self.__client.containers.run(
+            image,
+            name=name,
+            restart_policy={"Name": "always"},
+            publish_all_ports=True,
+        )
--- controllers/web_controller.py
+++ controllers/web_controller.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, template, static_file
+from bottle import Bottle, template, static_file, request, redirect
 from controllers.docker_controller import DockerController
 
 server = Bottle()
@@ -17,6 +17,20 @@ def home():
     return template('home', page='Home', containers=containers)
 
 
-@server.route('/create')
+@server.route('/select_image')
 def create():
-    return template('create', page='Create')
+    return template('select_image', page='Select Image')
+
+
+@server.route('/configure_container', method='POST')
+def configure_container():
+    image = request.forms.get('image')
+    return template('configure_container', page='Configure Container', image=image)
+
+
+@server.route('/create', method='POST')
+def create():
+    name = request.forms.get('name')
+    image = request.forms.get('image')
+    docker_controller.run(image, name)
+    return redirect('/')
